39b36aba9cd71ddb3e67db288fc4b8b230743bb4
ScaleMP <34648225+ScaleMP@users.noreply.github.com>
2018-01-28 12:01:32 +0200

    Add files via upload

---
+++ containers_ofbench/benchmark.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+base=/data/container_ofbench
+
+function test {
+  ((time docker run --entrypoint "" -v $base/bench/:/home/openfoam/bench openfoam/openfoam5-paraview54 /home/openfoam/bench/bench.sh) &> $1) &
+}
+
+if [ $# -eq 0 ]; then echo "Usage: ./benchmark [num containers]"; exit; fi
+
+num=$RANDOM
+dir=$base/results/bench.$1.$num
+
+mkdir $dir
+
+for ((i=1; i<=$1; i++))
+do
+  test $dir/bench.$i
+done
+
