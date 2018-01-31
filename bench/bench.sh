591501cd76cb55e613bb7f32cba91a3b028d4120
ScaleMP <34648225+ScaleMP@users.noreply.github.com>
2018-01-31 13:29:23 +0200

    Update bench.sh

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export MALLOC_TOP_PAD_=$((20*1024*1024))
+export MALLOC_TRIM_THRESHOLD_=$((20*1024*1024))
+export MALLOC_MMAP_MAX_=0
+
 . /opt/openfoam5/etc/bashrc
 
 cd /home/openfoam/bench/
@@ -11,7 +15,7 @@ mkdir bench$num
 cd bench$num
 ln -s ../SimpleBenchMarkLarge/* .
 
-sleep 60
+#sleep 60
 
 echo "#Begin @ $num"
 date
@@ -23,4 +27,3 @@ date
 
 cd ..
 rm -fr bench$num
-
