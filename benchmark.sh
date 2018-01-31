73ad5818f59834ebbfcf71da159f645483f3035e
ScaleMP <34648225+ScaleMP@users.noreply.github.com>
2018-01-31 13:28:41 +0200

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,14 @@
 
 base=/data/containers_openfoam
 
+# configrure nna=on
+for fname in `echo -n /usr/local/bin/????version | sed 's/^\(.*\)\(\/usr\/local\/bin\/vsmpversion\)\(.*\)$/\1\3 \2/'`; do
+        strings $fname 2>&1 | grep -iq "vsmpversion"
+        if [ $? == 0 ]; then TOOL=`basename $fname | awk '{print substr($0,0,4)}'`; break; fi
+done
+
+${TOOL}ctl --nna=on
+
 function test {
   ((time docker run --entrypoint "" -v $base/bench/:/home/openfoam/bench openfoam/openfoam5-paraview54 /home/openfoam/bench/bench.sh) &> $1) &
 }
@@ -16,5 +24,15 @@ mkdir $dir
 for ((i=1; i<=$1; i++))
 do
   test $dir/bench.$i
+  sleep 10
 done
 
+#Wait for container(s) to complete
+while :
+do
+sleep 10
+count=`docker ps | grep openfoam | wc -l`
+if [ $count == 0 ]; then
+        break
+fi
+done
